f12db3ed06f44f3d6d2e25b4b4dcdf919da218e6
Anna Kudriashova <amarenoru@gmail.com>
2024-08-03 20:28:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const leftParts = bracketsConfig.map(config => config[0]);
+  const rightParts = bracketsConfig.map(config => config[1]);
+  const stack = [];
+
+  for (let char of str) {
+      const leftIndex = leftParts.indexOf(char);
+      const rightIndex = rightParts.indexOf(char);
+
+      if (leftIndex === rightIndex) {
+          if (stack.includes(char)) {
+              stack.pop();
+          } else {
+              stack.push(char);
+          }
+      } else if (leftIndex !== -1) {
+          stack.push(char);
+      } else if (rightIndex !== -1 && stack.length > 0 && leftParts[rightIndex] === stack[stack.length - 1]) {
+          stack.pop();
+      } else {
+          return false;
+      }
+  }
+
+  return stack.length === 0;
 }
